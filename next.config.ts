2f8b583cb3723698cbb8bc693b9ca2bf13cef0ce
Tharindu N. Madhusanka <tharindu.nm@yahoo.com>
2025-08-20 03:14:44 +0530

    feat: update backend URL configuration for API rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
+const BACKEND_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
         source: "/api/v1/:path*",
-        destination: "http://localhost:3000/api/v1/:path*",
+        // destination: "http://localhost:3000/api/v1/:path*",
+        destination: `${BACKEND_URL}/api/v1/:path*`,
       },
     ];
   },
